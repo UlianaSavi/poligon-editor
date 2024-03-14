0ec691b57b1ef7b535a96cd54bdda85912c6e819
Uliana Savitskaua <uljasha.sav@gmail.com>
2024-03-14 17:16:24 +0600

    add map from mapbox

--- src/app/shared/components/map/map.component.ts
+++ src/app/shared/components/map/map.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import * as MapboxDraw from '@mapbox/mapbox-gl-draw';
+import { MAPBOX_TOKEN } from 'src/constants';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
+  private map: mapboxgl.Map | null = null;
+  private style = 'mapbox://styles/mapbox/streets-v11';
+  private lat = 37.75;
+  private lng = -122.41;
 
-}
+  constructor() { }
+  ngOnInit() {
+    this.map = new mapboxgl.Map({
+      accessToken: MAPBOX_TOKEN,
+      container: 'map',
+      zoom: 13,
+      center: [this.lng, this.lat]
+    });
+
+    this.map.addControl(new mapboxgl.NavigationControl());
+    this.map.addControl(new mapboxgl.FullscreenControl());
+
+    const draw = new MapboxDraw({
+      displayControlsDefault: false,
+      controls: {
+          polygon: true,
+          trash: true
+      },
+      defaultMode: 'draw_polygon'
+    });
+    this.map.addControl(draw);
+  }}
---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const MAPBOX_TOKEN = 'pk.eyJ1IjoibWFyb29uZWRpb25lIiwiYSI6ImNqdmp0MzB1azBpcDAzem1naHZwMjNndGIifQ.65nvvRg9QeFUV2c6b9W4Vw';
